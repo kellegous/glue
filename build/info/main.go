f5cbfe022587ef98fcb208e87edd977147ff6fd3
Kelly Norton <kellegous@gmail.com>
2025-03-26 20:31:32 -0400

    build/info iteration

--- build/info/main.go
+++ build/info/main.go
@@ -1,19 +1,55 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/base64"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
+	"text/template"
 
 	"github.com/kellegous/glue/build"
 )
 
+func applyFormat(s *build.Summary, format string) ([]byte, error) {
+	tpl, err := template.New("format").Parse(format)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf bytes.Buffer
+
+	params := map[string]any{
+		"SHA":         s.SHA,
+		"CommitTime":  s.CommitTime,
+		"Name":        s.Name,
+		"sha":         s.SHA,
+		"commit_time": s.CommitTime,
+		"name":        s.Name,
+	}
+
+	if err := tpl.Execute(&buf, params); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
 func main() {
 	var encode bool
-	flag.BoolVar(&encode, "encode", false, "output the summary as a base64 encoded string")
+	var format string
+	flag.BoolVar(
+		&encode,
+		"encode",
+		false,
+		"output the summary as a base64 encoded string")
+	flag.StringVar(
+		&format,
+		"format",
+		"",
+		"a text/template expression to format the output")
 	flag.Parse()
 
 	s, err := build.ReadSummaryFromGit(context.Background())
@@ -21,14 +57,22 @@ func main() {
 		log.Fatal(err)
 	}
 
-	b, err := json.Marshal(s)
-	if err != nil {
-		log.Fatal(err)
+	var out []byte
+	if format != "" {
+		out, err = applyFormat(s, format)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		out, err = json.Marshal(s)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	if encode {
-		fmt.Printf("%s\n", base64.StdEncoding.EncodeToString(b))
+		fmt.Printf("%s\n", base64.StdEncoding.EncodeToString(out))
 	} else {
-		fmt.Printf("%s\n", b)
+		fmt.Printf("%s\n", out)
 	}
 }
