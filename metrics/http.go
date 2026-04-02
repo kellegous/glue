5cd10c802afd17be5e9d2e487acf026cb632e0f4
Kelly Norton <kellegous@gmail.com>
2026-04-01 20:13:47 -0400

    add http metrics

---
+++ metrics/http.go
@@ -0,0 +1,27 @@
+package metrics
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	httpRequestsTotal = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "Total number of HTTP requests.",
+	}, []string{"path", "method", "code"})
+
+	httpDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "http_request_duration_seconds",
+		Help: "Duration of HTTP requests.",
+	}, []string{"path", "method"})
+)
+
+func ForHTTP(mux *http.ServeMux) http.Handler {
+	mux.Handle("/metrics", promhttp.Handler())
+	return promhttp.InstrumentHandlerDuration(httpDuration,
+		promhttp.InstrumentHandlerCounter(httpRequestsTotal, mux))
+}
